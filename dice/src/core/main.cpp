60a84f506f2cc88d4097d349562f1ee64a9c4844
Kaapo Moilanen <kaapomo@gmail.com>
2021-06-20 19:27:40 +0300

    Setup new project for fun times

---
+++ dice/src/core/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main(int argc, char** argv)
+{
+	std::string window_title = "Conquest Multiplayer";
+	int window_width = 1600;
+	int window_height = 900;
+	float fps_target = 600000.0f;
+	bool v_sync = false;
+
+
+
+	return 0;
+}
